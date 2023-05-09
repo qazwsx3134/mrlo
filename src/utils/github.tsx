5ebf41314a3570b2d604ee2841672f6218933791
Peter Tsai <qazwsx3134@gmail.com>
2023-05-09 10:54:35 +0800

    feat: add image uitl

---
+++ src/utils/github.tsx
@@ -0,0 +1,15 @@
+// Detect the url is Github pages or not
+export const isGithub = (url: string) => {
+  return url.includes("github.io");
+};
+
+// transform the url of image to Github pages for bug issue
+export const transformUrl =
+  (subPath: string) => (currentOrigin: string, imgUrl: string) => {
+    if (isGithub(currentOrigin)) {
+      return `${subPath}${imgUrl}`;
+    }
+    return imgUrl;
+  };
+
+export const transformToMrlo = transformUrl("mrlo/");
